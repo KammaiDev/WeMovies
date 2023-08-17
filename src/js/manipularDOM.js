9ea7183a10d07fb01a973ddfc0e24a53174d0265
Felipe Augusto Bonfim de Almeida <mavroscode@gmail.com>
2023-08-17 23:54:27 +0000

    test commited

--- src/js/manipularDOM.js
+++ src/js/manipularDOM.js
@@ -1,3 +1,35 @@
 const arrow = document.querySelector('#setinhaRoll')
-const bottomHeader = document.querySelector('#bottom-header')
+const arrowImg = document.querySelector('#setaBaixo')
+const header = document.querySelector('#fundoNav')
+
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos < currentScrollPos || prevScrollPos > currentScrollPos ) {
+    arrow.style.display = 'flex';
+    arrow.style.top = '0';
+  } else {
+    header.classList.add('hidden');
+  }
+
+  prevScrollPos = currentScrollPos;
+});
+
+arrowImg.addEventListener('click', () => {
+  header.style.position = 'fixed'
+  header.style.top = '0';
+  arrow.style.position = 'relative';
+});
+
+if (currentScrollPos === 0) {
+    arrow.style.display = 'none';
+    arrow.style.top = '0'; 
+}
+
+
+
+
+
 
