9387b377fbc8a7f5648ede902b15e449cce06989
Felipe Augusto Bonfim de Almeida <mavroscode@gmail.com>
2023-08-17 05:44:48 +0000

    save

---
+++ src/js/manipularDOM.js
@@ -0,0 +1,7 @@
+const headerBottom = document.getElementById('bottom-header')
+
+
+headerBottom.addEventListener('mouseover', () =>{
+    const divFundo = document.getElementById('fundoNav')
+        divFundo.classList.toggle('descer')
+    })
