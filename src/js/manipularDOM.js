e23f62130366f75b502b4677cebfd28350e3f046
Felipe Augusto Bonfim de Almeida <mavroscode@gmail.com>
2023-08-17 22:25:43 +0000

    Co-authored-by: Lorenzo Lima <Lorenzo-lima@users.noreply.github.com>

--- src/js/manipularDOM.js
+++ src/js/manipularDOM.js
@@ -1,8 +1,5 @@
 const headerBottom = document.getElementById('bottom-header').addEventListener('mouseover', () =>{
     const divFundo = document.getElementById('fundoNav')
-        divFundo.classList.add('descer')
-
-        setTimeout(() => {
-            divFundo.classList.remove('descer')
-        },5000)
+        //divFundo.classList.add('descer')
     })
+
