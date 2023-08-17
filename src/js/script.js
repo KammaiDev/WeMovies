b855ee9396665cc8222e2bd0f09fb017a4fa074f
Felipe Augusto Bonfim de Almeida <mavroscode@gmail.com>
2023-08-17 02:19:03 +0000

    MANGA

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,13 @@
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWM2Njg4NzVjMTg4OTVhYWZlN2YxNTA0ZGYyNGFlZCIsInN1YiI6IjY0ZGQ3ODQ1Yjc3ZDRiMTEzZmM1ODc5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.57UrtbuGonCbHNYOkl3j1QtjOmloEpALXtuTOT1zhRI'
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWM2Njg4NzVjMTg4OTVhYWZlN2YxNTA0ZGYyNGFlZCIsInN1YiI6IjY0ZGQ3ODQ1Yjc3ZDRiMTEzZmM1ODc5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.57UrtbuGonCbHNYOkl3j1QtjOmloEpALXtuTOT1zhRI'
     }
-  };
-  
-  fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+};
+
+
+fetch('https://api.themoviedb.org/3/movie/changes?page=1', options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
